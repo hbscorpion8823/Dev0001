a8c4f4fa16cc2b0930603203dc23ac0a6b2f06dd
hbscorpion8823 <hbculture@jcom.zaq.ne.jp>
2023-02-10 14:05:16 +0900

    getTextureの引数が何であっても正確な値を出すように修正

--- textureutil.py
+++ textureutil.py
@@ -1,8 +1,9 @@
 from kivy.uix.image import Image
+from decimal import Decimal
 
 
 class TextureUtil:
     @staticmethod
     def getTexture(img, region):
         tex = img.texture
-        return tex.get_region(region[0], region[1], region[2], region[3])
+        return tex.get_region(Decimal(str(region[0])), Decimal(str(region[1])), Decimal(str(region[2])), Decimal(str(region[3])))
